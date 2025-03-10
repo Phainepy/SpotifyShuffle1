f25a4010b279e7f8212ccef8a4be952f71464e4b
sesame <phainepy@gmail.com>
2025-03-09 19:05:22 -0700

    Looks bad - adding creation of playlist.

--- spotifyshuffle.py
+++ spotifyshuffle.py
@@ -1,50 +1,35 @@
 #import
 from secretcreds import *
 import requests
-import base64
 import spotipy
-
-'''
-#Get the Access Token, client credentials flow though. It's too simple. 
-
-def get_access_token(CLIENT_ID, CLIENT_SECRET):
-    auth_string = f"{CLIENT_ID}:{CLIENT_SECRET}"
-    auth_bytes = auth_string.encode("ascii")
-    auth_base64 = base64.b64encode(auth_bytes).decode("ascii")
-
-    url = "https://accounts.spotify.com/api/token"
-    headers = {
-        "Authorization": f"Basic {auth_base64}",
-        "Content-Type": "application/x-www-form-urlencoded"
-    }
-    data = {"grant_type": "client_credentials"}
-    result = requests.post(url, headers=headers, data=data)
-    json_result = result.json()
-    return json_result["access_token"]
-
-access_token = get_access_token(CLIENT_ID, CLIENT_SECRET)
-
-
-#Make API Requests
-def get_artist_info(artist_name, access_token):
-    url = f"https://api.spotify.com/v1/search?q={artist_name}&type=artist"
-    headers = {"Authorization": f"Bearer {access_token}"}
-    result = requests.get(url, headers=headers)
-    json_result = result.json()
-    return json_result
-
-artist_name = "The Beatles"
-artist_info = get_artist_info(artist_name, access_token)
-print(artist_info)
-'''
-
+import json
 from spotipy.oauth2 import SpotifyOAuth
 
 SCOPE = "playlist-read-private playlist-modify-public playlist-modify-private user-library-modify playlist-read-collaborative"
 
 
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID, client_secret=CLIENT_SECRET,redirect_uri=redirect_uri,scope=SCOPE))
-playlists = sp.current_user_playlists()
 
-for playlist in playlists['items']:
-    print (playlist['name'])
+playlist_id = "3H9t666C6120bwnUvKzWwe"
+results = sp.playlist(playlist_id)
+print(json.dumps(results, indent=4))
+
+
+print("Song - Artist - Album - id\n")
+
+for item in results['tracks']['items']:
+    track_id = item['track']['id']
+    print(
+        item['track']['name'] + ' - ' +
+        item['track']['artists'][0]['name'] + ' - ' +
+        item['track']['album']['name'] + ' - ' +
+        item['track']['id']
+    )
+
+
+#'''
+sp.user_playlist_create(user="76zzjj3qikd0y3sw7ulm32jue",name="TrueShuffle",description="Uses Spotify API for creation",public=True,)
+sp.user_playlist_add_tracks(user="76zzjj3qikd0y3sw7ulm32jue",playlist_id="0X2mrBCmoTDCU4nv9rjWrl",tracks=my_list_from_string)
+
+#Logic= If playlist called TrueShuffle Exists
+#'''
