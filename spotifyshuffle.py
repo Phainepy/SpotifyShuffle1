5064e7a5b121b7654e1e01ef1d043942b353ee83
sesame <phainepy@gmail.com>
2025-04-23 23:01:04 -0500

    It shuffles now! Thanks Random library.

--- spotifyshuffle.py
+++ spotifyshuffle.py
@@ -3,6 +3,7 @@ from secretcreds import *
 import requests
 import spotipy
 import json
+import random
 from spotipy.oauth2 import SpotifyOAuth
 
 SCOPE = "playlist-read-private playlist-modify-public playlist-modify-private user-library-modify playlist-read-collaborative"
@@ -14,11 +15,14 @@ playlist_id = "3H9t666C6120bwnUvKzWwe"
 results = sp.playlist(playlist_id)
 print(json.dumps(results, indent=4))
 
+#instantiating An empty list.
+my_list_from_string = [] 
 
 print("Song - Artist - Album - id\n")
 
 for item in results['tracks']['items']:
     track_id = item['track']['id']
+    my_list_from_string.append (track_id)
     print(
         item['track']['name'] + ' - ' +
         item['track']['artists'][0]['name'] + ' - ' +
@@ -26,10 +30,8 @@ for item in results['tracks']['items']:
         item['track']['id']
     )
 
+random.shuffle(my_list_from_string)
 
-#'''
-sp.user_playlist_create(user="76zzjj3qikd0y3sw7ulm32jue",name="TrueShuffle",description="Uses Spotify API for creation",public=True,)
-sp.user_playlist_add_tracks(user="76zzjj3qikd0y3sw7ulm32jue",playlist_id="0X2mrBCmoTDCU4nv9rjWrl",tracks=my_list_from_string)
-
-#Logic= If playlist called TrueShuffle Exists
-#'''
+#Create the playlist, add tracks to playlist. New PLyalist ID comes from the response.
+resp = sp.user_playlist_create(user="76zzjj3qikd0y3sw7ulm32jue",name="TrueShuffle",description="Uses Spotify API for creation",public=True,)
+sp.user_playlist_add_tracks(user="76zzjj3qikd0y3sw7ulm32jue",playlist_id= resp["id"] ,tracks=my_list_from_string)
